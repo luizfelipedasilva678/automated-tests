4d984074e0011eab01d44954f35c4d6a25d9ba24
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-11-24 22:52:42 -0300

    order test

---
+++ src/dip/classes/order.spec.ts
@@ -0,0 +1,109 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Order } from './order';
+import { ShoppingCartProtocol } from './interfaces/shop-cart-protocol';
+import { CartItem } from './interfaces/cart-item';
+import { MessagingProtocol } from './interfaces/messagin-protocol';
+import { PersistenceProtocol } from './interfaces/persistence-protocol';
+import { CustomerOrder } from './interfaces/customer-protocol';
+
+class ShoppingCartMock implements ShoppingCartProtocol {
+  totalWidthDiscount(): number {
+    return 1;
+  }
+  get items(): Readonly<CartItem[]> {
+    return [];
+  }
+  addItem(item: CartItem): void {}
+  removeItem(index: number): void {}
+  total(): number {
+    return 1;
+  }
+  totalWithDicount(): number {
+    return 2;
+  }
+  isEmpty(): boolean {
+    return false;
+  }
+  clear(): void {}
+}
+
+class MessagingMock implements MessagingProtocol {
+  sendMessage() {}
+}
+
+class PersistencyMock implements PersistenceProtocol {
+  saveOrder() {}
+}
+
+class CustomerMock implements CustomerOrder {
+  getName() {
+    return '';
+  }
+  getIDN() {
+    return '';
+  }
+}
+
+const createSut = () => {
+  const shoppingCartMock = new ShoppingCartMock();
+  const messagingMock = new MessagingMock();
+  const persistencyMock = new PersistencyMock();
+  const customerMock = new CustomerMock();
+  const sut = new Order(
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+    customerMock,
+  );
+
+  return {
+    sut,
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+  };
+};
+
+describe('Order', () => {
+  it('should not checkout if cart is empty', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest
+      .spyOn(shoppingCartMock, 'isEmpty')
+      .mockReturnValueOnce(true);
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe('open');
+  });
+
+  it('should checkout if cart is not empty', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest
+      .spyOn(shoppingCartMock, 'isEmpty')
+      .mockReturnValueOnce(false);
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe('closed');
+  });
+
+  it('should send an email to customer', () => {
+    const { sut, messagingMock } = createSut();
+    const messagingMockSpy = jest.spyOn(messagingMock, 'sendMessage');
+    sut.checkout();
+    expect(messagingMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should save order', () => {
+    const { sut, persistencyMock } = createSut();
+    const persistencyMockSpy = jest.spyOn(persistencyMock, 'saveOrder');
+    sut.checkout();
+    expect(persistencyMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should clear cart', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest.spyOn(shoppingCartMock, 'clear');
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+  });
+});
