33d62ddaa66e705f7c46c50cb2c4420526a4bfaa
Luiz Felipe <luizfelipedasilva678@gmail.com>
2021-11-24 21:11:34 -0300

    spyOn

--- src/dip/classes/persistence.spec.ts
+++ src/dip/classes/persistence.spec.ts
@@ -1,13 +1,28 @@
-describe('Testando alguma coisa', () => {
-  it('Descrição do test (IT) ', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistence } from './persistence';
 
-describe('Testando alguma coisa', () => {
-  test('Descrição do teste (Teste)', () => {
-    const nome = 'Luiz';
-    expect(nome).toBe('Luiz');
+describe('Persistence', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return undefined', () => {
+    //System under test
+    const sut = new Persistence();
+
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with Order save with success...', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Order save with success...');
   });
 });
